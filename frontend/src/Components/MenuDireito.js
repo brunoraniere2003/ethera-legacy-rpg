d9ec043cb9304a72bb9792f88ccb1486d65d2b8a
Bruno Raniere <brunoraniere2@gmail.com>
2024-10-18 01:28:07 -0300

    Add MenuEsquerdo, MenuDireito, and Footer components

---
+++ frontend/src/Components/MenuDireito.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MenuDireito = () => {
+  return (
+    <div>
+      <h2>Menu Direito</h2>
+      {/* Componentes dinâmicos como combate, loja, etc. */}
+    </div>
+  );
+};
+
+export default MenuDireito;
---
+++ frontend/src/Components/MenuEsquerdo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MenuEsquerdo = () => {
+  return (
+    <div>
+      <h2>Menu Esquerdo</h2>
+      {/* Componentes de inventário e status do jogador */}
+    </div>
+  );
+};
+
+export default MenuEsquerdo;
